2148a841954a01eb12414ce07f2c72fb7ca30019
GbemiAjagbe <dajagbe357@stu.ui.edu.ng>
2022-09-08 01:19:57 -0400

    working on google auth and passport

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require ('express')
 const dotenv = require ('dotenv')
 const morgan = require ('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 const homepage = require('./routes/homepage')
+//import { login, home, register, products, sales, receipts, suppliers } from './routes';
 
 //load config 
 dotenv.config({ path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -22,6 +28,17 @@ if (process.env.NODE_ENV === 'development') {
 //Handlebars
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static folder 
 app.use(express.static(path.join(__dirname, 'public')))
